1b9a25135fd095983ccac8060b7e9328b72b77c6
BAHODIR <bahodirk101@gmail.com>
2024-02-27 15:31:45 +0500

    Login

---
+++ lib/presenter/screens/login/sms_code_succses.dart
@@ -0,0 +1,272 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SMSSucssesPage extends StatefulWidget {
+  const SMSSucssesPage({super.key});
+
+  @override
+  State<SMSSucssesPage> createState() => _SMSSucssesPageState();
+}
+
+class _SMSSucssesPageState extends State<SMSSucssesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          children: [
+            Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(left: 20, top: 30),
+                    width: 24,
+                    height: 24,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Color(0xff636C79)),
+                    child: Center(
+                        child: Icon(Icons.keyboard_arrow_left_outlined,
+                            color: Colors.white)),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Center(
+                        child: Image.asset(
+                          "assets/images/app_image.png",
+                          height: 64,
+                          width: 64,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Center(
+                        child: Padding(
+                          padding: EdgeInsets.only(left: 20, top: 30),
+                          child: Text(
+                            "Tasdiqlash kodi",
+                            style: GoogleFonts.montserrat(
+                                fontSize: 28, fontWeight: FontWeight.w700),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Center(
+                        child: Padding(
+                          padding: EdgeInsets.only(left: 20, top: 16),
+                          child: Text(
+                            "Quyidagi telefon raqamiga sms-kod yuborildi!",
+                            style: GoogleFonts.montserrat(
+                                fontSize: 12, fontWeight: FontWeight.w700,
+                              color: Colors.black
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Center(
+                        child: Padding(
+                          padding: EdgeInsets.only(left: 20, top: 16),
+                          child: Text(
+                            "+998 91 791 11 22",
+                            style: GoogleFonts.montserrat(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w700,
+                                color: Colors.black
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+
+
+                  SizedBox(
+                    height: 20,
+                  ),
+
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20, right: 20),
+                    child: TextField(
+                      maxLength: 4,
+                      textAlign: TextAlign.center,
+                      keyboardType: TextInputType.phone,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(14),
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color(0xffF1F2F4), // Border rangi
+                          ),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(14),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+
+
+
+                  SizedBox(height: 10,),
+                  
+
+                  Center(
+                    child: Container(
+                      margin: EdgeInsets.only(left: 20, right: 20),
+                      height: 40,
+                      width: 60,
+                      decoration: BoxDecoration(
+                          color: Colors.black26,
+                          borderRadius: BorderRadiusDirectional.circular(14)),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+
+                          Text(
+                            "00:17",
+                            style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.black
+                            ),
+                          )
+
+
+                        ],
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(height: 22,),
+
+                  Center(
+                    child: Container(
+                      margin: EdgeInsets.only(left: 20, right: 20),
+                      height: 40,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadiusDirectional.circular(14)),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+
+                          Image.asset("assets/update.png"),
+
+                          SizedBox(width: 8,),
+
+                          Text(
+                            "Kodni qayta yuboring",
+                            style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.black
+                            ),
+                          )
+
+
+                        ],
+                      ),
+                    ),
+                  ),
+
+
+
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 20, right: 20),
+                    height: 50,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadiusDirectional.circular(30)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Tasdiqlash",
+                          style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500,
+                              color: Color(0xffF1F2F4)),
+                        )
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 16, right: 16, top: 90),
+                    child: Row(
+                      children: [
+                        Text(
+                          "Mobil ilovadan ro‘yxatdan o‘tish orqali, siz  ",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 10,
+                          ),
+                        ),
+                        Text(
+                          "Foydalanuvchi kelishuvi",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 10, color: Colors.blue),
+                        ),
+                        Text(
+                          "va",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 10, color: Colors.blue),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 16, right: 16),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Maxfiylik siyosati ",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 10, color: Colors.blue),
+                        ),
+                        Text(
+                          "shartlariga rozi ekanligingizni tasdiqlaysiz.",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 10,
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
